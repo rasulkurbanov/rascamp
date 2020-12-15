1348efbed75586cc61222e0f351947eb96cc777e
rasulkurbanov <rasuljohn139@gmail.com>
2020-12-16 00:19:09 +0500

    Added bcrypt to hash passwords as a middleware

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,7 +7,15 @@ const User = require('../models/User')
 //@route POST /api/v1/auth/register
 //access PUBLIC
 exports.register = asyncHandler(async(req, res, next) => {
-  res.status(201).json({success: true})
-  console.log(req.body)
+  const {name, email, role, password } = req.body
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  })
+
+  res.status(201).json({success: true, data: user})
 })
 
